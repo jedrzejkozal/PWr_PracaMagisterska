2078948e680f81b42afd65aa805236f201479a5d
jedrzejkozal <jedrzej.kozal@gmail.com>
2019-01-03 09:49:49 +0100

    some functions moved to utils

--- experiments/keras_lstm_tutorial/main.py
+++ experiments/keras_lstm_tutorial/main.py
@@ -1,76 +1,26 @@
 #source: http://adventuresinmachinelearning.com/keras-lstm-tutorial/
 
-import collections
 import os
-import tensorflow as tf
 from keras.models import Sequential, load_model
 from keras.layers import Dense, Activation, Embedding, Dropout, TimeDistributed
 from keras.layers import LSTM
 from keras.optimizers import Adam
 from keras.callbacks import ModelCheckpoint
 import numpy as np
-import argparse
 
 from KerasBatchGenerator import KerasBatchGenerator
+from utils import *
 
 repo_path = os.path.dirname(os.path.realpath(__file__))
 data_path = repo_path + "/../datasets/simple-examples/data"
 
 
-def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('run_opt', type=int, default=1, help='An integer: 1 to train, 2 to test')
-    parser.add_argument('--data_path', type=str, default=data_path, help='The full path of the training data')
-    return parser.parse_args()
-
-args = parse_args()
+args = parse_args(data_path)
 if args.data_path:
     data_path = args.data_path
 
-def read_words(filename):
-    with tf.gfile.GFile(filename, "r") as f:
-        #return f.read().decode("utf-8").replace("\n", "<eos>").split()
-        return f.read().replace("\n", "<eos>").split()
 
-
-def build_vocab(filename):
-    data = read_words(filename)
-
-    counter = collections.Counter(data)
-    count_pairs = sorted(counter.items(), key=lambda x: (-x[1], x[0]))
-
-    words, _ = list(zip(*count_pairs))
-    word_to_id = dict(zip(words, range(len(words))))
-
-    return word_to_id
-
-
-def file_to_word_ids(filename, word_to_id):
-    data = read_words(filename)
-    return [word_to_id[word] for word in data if word in word_to_id]
-
-
-def load_data():
-    # get the data paths
-    train_path = os.path.join(data_path, "ptb.train.txt")
-    valid_path = os.path.join(data_path, "ptb.valid.txt")
-    test_path = os.path.join(data_path, "ptb.test.txt")
-
-    # build the complete vocabulary, then convert text data to list of integers
-    word_to_id = build_vocab(train_path)
-    train_data = file_to_word_ids(train_path, word_to_id)
-    valid_data = file_to_word_ids(valid_path, word_to_id)
-    test_data = file_to_word_ids(test_path, word_to_id)
-    vocabulary = len(word_to_id)
-    reversed_dictionary = dict(zip(word_to_id.values(), word_to_id.keys()))
-
-    print(train_data[:5])
-    print(word_to_id)
-    print(vocabulary)
-    print(" ".join([reversed_dictionary[x] for x in train_data[:10]]))
-    return train_data, valid_data, test_data, vocabulary, reversed_dictionary
-
-train_data, valid_data, test_data, vocabulary, reversed_dictionary = load_data()
+train_data, valid_data, test_data, vocabulary, reversed_dictionary = load_data(data_path)
 
 num_steps = 30
 batch_size = 20
---
+++ experiments/keras_lstm_tutorial/utils.py
@@ -0,0 +1,54 @@
+import os
+import collections
+import tensorflow as tf
+import argparse
+
+
+def parse_args(data_path):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('run_opt', type=int, default=1, help='An integer: 1 to train, 2 to test')
+    parser.add_argument('--data_path', type=str, default=data_path, help='The full path of the training data')
+    return parser.parse_args()
+
+
+def read_words(filename):
+    with tf.gfile.GFile(filename, "r") as f:
+        return f.read().replace("\n", "<eos>").split()
+
+
+def build_vocab(filename):
+    data = read_words(filename)
+
+    counter = collections.Counter(data)
+    count_pairs = sorted(counter.items(), key=lambda x: (-x[1], x[0]))
+
+    words, _ = list(zip(*count_pairs))
+    word_to_id = dict(zip(words, range(len(words))))
+
+    return word_to_id
+
+
+def file_to_word_ids(filename, word_to_id):
+    data = read_words(filename)
+    return [word_to_id[word] for word in data if word in word_to_id]
+
+
+def load_data(data_path):
+    # get the data paths
+    train_path = os.path.join(data_path, "ptb.train.txt")
+    valid_path = os.path.join(data_path, "ptb.valid.txt")
+    test_path = os.path.join(data_path, "ptb.test.txt")
+
+    # build the complete vocabulary, then convert text data to list of integers
+    word_to_id = build_vocab(train_path)
+    train_data = file_to_word_ids(train_path, word_to_id)
+    valid_data = file_to_word_ids(valid_path, word_to_id)
+    test_data = file_to_word_ids(test_path, word_to_id)
+    vocabulary = len(word_to_id)
+    reversed_dictionary = dict(zip(word_to_id.values(), word_to_id.keys()))
+
+    print(train_data[:5])
+    print(word_to_id)
+    print(vocabulary)
+    print(" ".join([reversed_dictionary[x] for x in train_data[:10]]))
+    return train_data, valid_data, test_data, vocabulary, reversed_dictionary
