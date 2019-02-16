7499035294db23ea4afe293780fc8ef489741505
jedrzejkozal <jedrzej.kozal@gmail.com>
2019-02-16 16:04:17 +0100

    UT refactor

--- ReNet/SimpleReNet_test.py
+++ ReNet/SimpleReNet_test.py
@@ -17,33 +17,33 @@ class TestSimpleReNet(object):
 
     @pytest.fixture
     def simple_data_x(self):
-        num_samples = 60
-        x = np.zeros((num_samples, 10, 10, 1), dtype=np.uint8)
+        self.num_samples = 60
+        x = np.zeros((self.num_samples, 10, 10, 1), dtype=np.uint8)
         x[30:] = np.ones((10, 10, 1))
         return x
 
 
     @pytest.fixture
     def simple_data_y(self):
-        y = np.zeros((60), dtype=np.uint8)
+        self.num_samples = 60
+        y = np.zeros((self.num_samples), dtype=np.uint8)
         y[30:] = 1
-        print(y)
         return y
 
 
-    def test_model_output_for_2_classes_is_0_or_1(self, sut, simple_data_x,
+    def test_model_output_for_2_classes_shape_is_num_samples_x_2(self, sut, simple_data_x,
             simple_data_y):
         sut.fit(simple_data_x, simple_data_y, epochs=1, shuffle=False)
-        assert sut.predict(simple_data_x).all() == 0 or sut.predict(simple_data_x).all() == 1
+        result = sut.predict(simple_data_x)
+        assert result.shape == (self.num_samples, 2)
 
 
     def test_get_columns_outputs_valid_columns_shape(self, sut):
         arg = Input((10, 10, 1))
 
         for result in sut.get_columns(arg):
-            assert result.shape[1] == 10
-            assert result.shape[2] == 2
-            assert result.shape[3] == 1
+            result_shape = list(map(lambda x: int(x), result.shape[1:]))
+            assert result_shape == [10, 2, 1]
 
 
     def test_get_columns_generates_5_columns(self, sut):
