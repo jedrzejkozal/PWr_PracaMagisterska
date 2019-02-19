9fdec733bb65874a8b3540f3fa876ce35483baaf
jedrzejkozal <jedrzej.kozal@gmail.com>
2019-02-19 16:26:20 +0100

    rectangular patch UT

--- ReNet/ReNet_rectangular_patch_test.py
+++ ReNet/ReNet_rectangular_patch_test.py
@@ -2,20 +2,20 @@ from ReNetTest import *
 from SimpleReNet import *
 
 
-class TestSimpleReNet(ReNetTest):
+class TestRectangularPatchReNet(ReNetTest):
 
     @classmethod
     def setup(cls):
         cls.num_samples = 60
-        cls.img_width = 10
-        cls.img_height = 10
+        cls.img_width = 12
+        cls.img_height = 12
         cls.number_of_channels = 1
 
     @pytest.fixture
     def sut(self):
         self.__class__.setup()
-        self.w_p = 2
-        self.h_p = 2
+        self.w_p = 3
+        self.h_p = 4
         self.I = self.img_width // self.w_p
         self.J = self.img_height // self.h_p
         self.reNet_hidden_size = 1
