7639eb9dc4a244da0c20ef80803e13e1399761cd
jedrzejkozal <jedrzej.kozal@gmail.com>
2019-02-27 12:36:57 +0100

    mnist tests for Modified ReNet

--- ModifiedReNet/mnist/MnistModel.py
+++ ModifiedReNet/mnist/MnistModel.py
@@ -1,40 +1,38 @@
 import sys
 from keras import Model
+from keras.layers import Dense, Flatten
 
 sys.path.insert(0, "../")
-from ReNetLayer import *
+from HilbertLayer import *
+from ModifiedReNetLayer import *
 
 
-class MnistReproduction(Model):
+class MnistModel(Model):
 
     def __init__(self):
         super().__init__()
 
-        self.first_reNetLayer = ReNetLayer([[2, 2]], 128, #256
-                use_dropout=True, dropout_rate=0.2)
-        self.second_reNetLayer = ReNetLayer([[2, 2]], 128, #256
-                use_dropout=True, dropout_rate=0.2)
+        self.hilbert_layer = HilbertLayer()
+        self.first_reNetLayer = ModifiedReNetLayer(4, 128)
+        self.second_reNetLayer = ModifiedReNetLayer(4, 128)
 
         self.flatten = Flatten()
-        fully_conn_hidden_size = 2048 #4096
+        fully_conn_hidden_size = 2048
         self.first_dense = Dense(fully_conn_hidden_size, activation='relu')
-        self.first_dropout = Dropout(0.2)
         self.second_dense = Dense(fully_conn_hidden_size, activation='relu')
-        self.second_dropout = Dropout(0.2)
 
         num_classes = 10
         self.softmax = Dense(num_classes, activation='softmax')
 
 
     def call(self, inputs):
-        first_reNet_output = self.first_reNetLayer(inputs)
+        flat_imgs = self.hilbert_layer(inputs)
+        first_reNet_output = self.first_reNetLayer(flat_imgs)
         second_reNet_output = self.second_reNetLayer(first_reNet_output)
 
         x = self.flatten(second_reNet_output)
         x = self.first_dense(x)
-        x = self.first_dropout(x)
         x = self.second_dense(x)
-        x = self.second_dropout(x)
         x = self.softmax(x)
 
         return x
---
+++ ModifiedReNet/mnist/mnist.py
@@ -0,0 +1,88 @@
+import os
+import numpy as np
+from keras.utils import to_categorical
+from keras.datasets import mnist
+from keras.preprocessing.image import ImageDataGenerator
+from keras.callbacks import EarlyStopping
+from scipy.misc import imresize
+import sys
+sys.path.append('../Utils')
+#from SaveResults import *
+
+from MnistModel import *
+
+
+#model hyperparmeters:
+w_p = 2
+h_p = 2
+reNet_hidden_size = 1
+fully_conn_hidden_size = 1
+num_classes = 2
+
+
+#image parameters:
+num_classes = 10
+img_rows, img_cols = 28, 28
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+input_shape = (img_rows, img_cols, 1)
+
+x_train_reshaped = np.zeros((x_train.shape[0], 32, 32, 1))
+x_test_reshaped = np.zeros((x_test.shape[0], 32, 32, 1))
+
+for i in range(x_train.shape[0]):
+    #print("x_train[i].shape: ", x_train[i, :, :, 0].shape)
+    x_train_reshaped[i, :, :, 0] = imresize(x_train[i, :, :, 0], (32,32))
+
+for i in range(x_test.shape[0]):
+    x_test_reshaped[i, :, :, 0] = imresize(x_test[i, :, :, 0], (32,32))
+
+x_train, x_test = x_train_reshaped, x_test_reshaped
+
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+
+# convert class vectors to binary class matrices
+y_train = to_categorical(y_train, num_classes)
+y_test = to_categorical(y_test, num_classes)
+
+print("x_train: ", x_train.shape)
+print("x_train_single: ", x_train[0:1].shape)
+print("y_train: ", y_train.shape)
+print("y_train_single:", y_train[0:1].shape)
+
+x_train_single_ex = x_train[0:1]
+y_train_single_ex = y_train[0:1]
+
+shift = 3
+datagen = ImageDataGenerator(width_shift_range=shift, height_shift_range=shift)
+datagen.fit(x_train)
+
+
+model = MnistModel()
+model.compile(loss='categorical_crossentropy', optimizer='adam',
+        metrics=['categorical_accuracy'])
+
+#just for model to figure out what is the shape of input tensors
+#workaround for how keras fit_generator works
+model.fit(x_train_single_ex, y_train_single_ex,
+                epochs=1,
+                validation_data=(x_test, y_test),
+                callbacks=[EarlyStopping(monitor='val_loss', patience=5, verbose=1)]
+            )
+
+history = model.fit_generator(datagen.flow(x_train, y_train, batch_size=32),
+                epochs=1000,
+                steps_per_epoch=80,
+                validation_data=(x_test, y_test),
+                callbacks=[EarlyStopping(monitor='val_loss', patience=5, verbose=1)]
+            )
+
+
+#path = os.path.dirname(os.path.realpath(__file__))
+#print("path: ", path)
+#save = SaveResults(history, path)
