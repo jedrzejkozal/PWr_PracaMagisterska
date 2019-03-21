c843e842cef06bdb16b6216c6d37295426928044
jedrzejkozal <jedrzej.kozal@gmail.com>
2019-03-21 12:46:23 +0100

    mnist model params update

--- ReNet/Models/MnistReproduction/MnistModel.py
+++ ReNet/Models/MnistReproduction/MnistModel.py
@@ -10,17 +10,17 @@ def get_mnist_model():
 
     reNet_hidden_size = 256
     model.add(ReNetLayer([[2, 2]], reNet_hidden_size,
-            use_dropout=True, dropout_rate=0.2,
-            is_first_layer=False))
+            use_dropout=True, dropout_rate=0.1,
+            is_first_layer=True))
     model.add(ReNetLayer([[2, 2]], reNet_hidden_size,
-            use_dropout=True, dropout_rate=0.2))
+            use_dropout=True, dropout_rate=0.1))
 
     model.add(Flatten())
     fully_conn_hidden_size = 4096
     model.add(Dense(fully_conn_hidden_size, activation='relu'))
-    model.add(Dropout(0.2))
+    model.add(Dropout(0.1))
     model.add(Dense(fully_conn_hidden_size, activation='relu', name='features'))
-    model.add(Dropout(0.2))
+    model.add(Dropout(0.1))
 
     num_classes = 10
     model.add(Dense(num_classes, activation='softmax'))
