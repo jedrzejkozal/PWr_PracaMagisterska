a30aa83f67a437acc0cacdeb971af55c7511acd3
jedrzejkozal <jedrzej.kozal@gmail.com>
2019-03-22 13:50:40 +0100

    Load SVHN from .npy

--- ReNet/Utils/LoadSVHN.py
+++ ReNet/Utils/LoadSVHN.py
@@ -15,15 +15,17 @@ def load_mat_file(filename):
 
 def load_SVHN(path):
     x_train, y_train = load_mat_file(path+'train_32x32.mat')
-    x_test, y_test = load_mat_file(path+'test_32x32.mat')
     x_extra, y_extra = load_mat_file(path+'extra_32x32.mat')
 
+    #x_train = np.load(path+'x_train.npy')
+    #y_train = np.load(path+'y_train.npy')
+    #x_extra = np.load(path+'x_extra.npy')
+    #y_extra = np.load(path+'y_extra.npy')
+
     x_train = np.moveaxis(x_train, -1, 0)
-    x_test = np.moveaxis(x_test, -1, 0)
     x_extra = np.moveaxis(x_extra, -1, 0)
 
     y_train = np.squeeze(y_train)
-    y_test = np.squeeze(y_test)
     y_extra = np.squeeze(y_extra)
 
     x_concat = np.vstack([x_train, x_extra])
@@ -39,7 +41,16 @@ def load_SVHN(path):
             num_classes=10,
             labels=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 
+    del x_concat
+    del y_concat
+
     x_train = x_train[:543949]
     y_train = y_train[:543949]
+
+    x_test, y_test = load_mat_file(path+'test_32x32.mat')
+    #x_test = np.load(path+'x_test.npy')
+    #y_test = np.load(path+'y_test.npy')
+    x_test = np.moveaxis(x_test, -1, 0)
+    y_test = np.squeeze(y_test)
 
     return x_train, y_train, x_test, y_test
