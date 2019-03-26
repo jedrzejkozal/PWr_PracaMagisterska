c5a358588b480d84758263f56c5afe8507a8632b
jedrzejkozal <jedrzej.kozal@gmail.com>
2019-03-26 13:26:13 +0100

    removal of old .npy files

--- ReNet/SVHNdataset/convert_to_npy.py
+++ ReNet/SVHNdataset/convert_to_npy.py
@@ -16,19 +16,19 @@ def load_mat_file(filename):
 def save_matrix(matrix, filename):
     np.save(filename, matrix, allow_pickle=False)
 
-x_train, y_train = load_mat_file(join(path,'train_32x32.mat'))
+x_train, y_train = load_mat_file('train_32x32.mat')
 save_matrix(x_train, 'x_train.npy')
 save_matrix(y_train, 'y_train.npy')
 del x_train
 del y_train
 
-x_extra, y_extra = load_mat_file(join(path,'extra_32x32.mat'))
+x_extra, y_extra = load_mat_file('extra_32x32.mat')
 save_matrix(x_extra, 'x_extra.npy')
 save_matrix(y_extra, 'y_extra.npy')
 del x_extra
 del y_extra
 
-x_test, y_test = load_mat_file(join(path,'test_32x32.mat'))
+x_test, y_test = load_mat_file('test_32x32.mat')
 save_matrix(x_test, 'x_test.npy')
 save_matrix(y_test, 'y_test.npy')
 del x_test
