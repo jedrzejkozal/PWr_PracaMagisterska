85496fd15f52306b47709f0cd627b32f079e097b
jedrzejkozal <jedrzej.kozal@gmail.com>
2019-03-28 13:32:56 +0100

    .npz files generated

--- ReNet/SVHNdataset/convert_to_npy.py
+++ ReNet/SVHNdataset/convert_to_npy.py
@@ -14,22 +14,22 @@ def load_mat_file(filename):
 
 
 def save_matrix(matrix, filename):
-    np.save(filename, matrix, allow_pickle=True, fix_imports=False)
+    np.savez(filename, matrix)
 
 x_train, y_train = load_mat_file('train_32x32.mat')
-save_matrix(x_train, 'x_train.npy')
-save_matrix(y_train, 'y_train.npy')
+save_matrix(x_train, 'x_train')
+save_matrix(y_train, 'y_train')
 del x_train
 del y_train
 
 x_extra, y_extra = load_mat_file('extra_32x32.mat')
-save_matrix(x_extra, 'x_extra.npy')
-save_matrix(y_extra, 'y_extra.npy')
+save_matrix(x_extra, 'x_extra')
+save_matrix(y_extra, 'y_extra')
 del x_extra
 del y_extra
 
 x_test, y_test = load_mat_file('test_32x32.mat')
-save_matrix(x_test, 'x_test.npy')
-save_matrix(y_test, 'y_test.npy')
+save_matrix(x_test, 'x_test')
+save_matrix(y_test, 'y_test')
 del x_test
 del y_test
