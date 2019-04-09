5d5d164034135952b4cfb36f34eda8eb62df2456
jedrzejkozal <jedrzej.kozal@gmail.com>
2019-04-09 11:45:08 +0200

    tensorflow2.0 model test

--- ReNet/Models/SVHNReproduction/SVHNModel.py
+++ ReNet/Models/SVHNReproduction/SVHNModel.py
@@ -1,3 +1,4 @@
+"""
 from keras.models import Sequential
 
 from Models.ReNetLayer import *
@@ -26,5 +27,36 @@ def get_svhn_model():
 
     num_classes = 10
     model.add(Dense(num_classes, activation='softmax'))
+
+    return model
+"""
+
+
+from tf.keras import Sequential
+from Models.ReNetLayer import *
+from Models.InputMaskingLayer import *
+
+def get_svhn_model():
+    model = Sequential()
+    model.add(InputMaskingLayer(0.2))
+
+    reNet_hidden_size = 5 #256
+    model.add(ReNetLayer([[2, 2]], reNet_hidden_size,
+            use_dropout=True, dropout_rate=0.2,
+            is_first_layer=True))
+    model.add(ReNetLayer([[2, 2]], reNet_hidden_size,
+            use_dropout=True, dropout_rate=0.2))
+    model.add(ReNetLayer([[2, 2]], reNet_hidden_size,
+            use_dropout=True, dropout_rate=0.2))
+
+    model.add(tf.keras.layers.Flatten())
+    fully_conn_hidden_size = 20 #4096
+    model.add(tf.keras.layers.Dense(fully_conn_hidden_size, activation='relu'))
+    model.add(tf.keras.layers.Dropout(0.2))
+    model.add(tf.keras.layers.Dense(fully_conn_hidden_size, activation='relu', name='features'))
+    model.add(tf.keras.layers.Dropout(0.2))
+
+    num_classes = 10
+    model.add(tf.keras.layers.Dense(num_classes, activation='softmax'))
 
     return model
