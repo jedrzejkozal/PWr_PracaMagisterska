4d8443c9cfa495802700aa4333cdd8781b7e54b5
jedrzejkozal <jedrzej.kozal@gmail.com>
2019-04-23 20:28:35 +0200

    masking input update

--- ReNet/mnist.py
+++ ReNet/mnist.py
@@ -10,10 +10,6 @@ from os import makedirs
 from os.path import exists, join
 from shutil import rmtree
 
-from Utils.SaveResults import *
-from Utils.TensorBoardSaveSplits import *
-from Utils.SaveTensorBoardSprite import *
-from Utils.ImageGeneratorWithMasking import *
 from Models.MnistReproduction.MnistModel import *
 
 
@@ -44,24 +40,13 @@ y_train = to_categorical(y_train, num_classes)
 y_test = to_categorical(y_test, num_classes)
 
 #just for testing
-#x_train = x_train[:10]
-#y_train = y_train[:10]
-#x_test = x_test[:10]
-#y_test = y_test[:10]
-
-log_dir = 'TensorBoard_mnist_logs'
-rmtree(log_dir, ignore_errors=True)
-makedirs(log_dir)
-#save_sprites(x_test, log_dir)
+x_train = x_train[:10]
+y_train = y_train[:10]
+x_test = x_test[:10]
+y_test = y_test[:10]
 
 
-# save class labels to disk to color data points in TensorBoard accordingly
-#with open(join(log_dir, 'metadata.tsv'), 'w') as f:
-#    np.savetxt(f, y_test)
-
-
-#datagen = ImageDataGenerator(width_shift_range=[-2.0, 0.0, 2.0])
-datagen = ImageDataGeneratorWithMasking(width_shift_range=[-2.0, 0.0, 2.0])
+datagen = ImageDataGenerator(width_shift_range=[-2.0, 0.0, 2.0])
 datagen.fit(x_train)
 
 
@@ -79,25 +64,52 @@ model.fit(x_train_single_ex, y_train_single_ex, epochs=1)
 model.summary()
 
 batch_size = 30
-history = model.fit_generator(datagen.flow(x_train, y_train, batch_size=batch_size),
-        epochs=10,
-        steps_per_epoch=np.ceil(x_train.shape[0] / batch_size),
-        validation_data=(x_test, y_test),
-        callbacks=[EarlyStopping(monitor='val_loss', patience=20, verbose=1),
-                #LambdaCallback(on_epoch_end=lambda x, y: model.layers[0].generate_mask()),
-                TensorBoardSaveSplits(log_dir=log_dir,
-                        #splits_size=[28,28],
-                        #splits_path='sprite.png',
-                        batch_size=batch_size,
-                        histogram_freq=1,
-                        write_images=True,
-                        write_grads=False,
-                        #embeddings_freq=1,
-                        #embeddings_layer_names=['features'],
-                        #embeddings_metadata='metadata.tsv',
-                        #embeddings_data=x_test
-                )
-            ]
-    )
 
-print(history.history)
+def mask_input(x):
+    print("mask_input call")
+    mask = generate_mask_with_prob(0.2)
+
+    tmp = np.copy(x)
+    for i in range(x.shape[0]):
+        tmp[i][mask] = -100.0
+    return tmp
+
+def generate_mask_with_prob(p):
+    rand_matrix = get_random_binomial(p, get_patched_img_size())
+    expand_matrix = get_expand_matrix(img_rows // 2)
+    single_dim_mask = np.transpose(expand_matrix) @ rand_matrix @ expand_matrix
+
+    all_dims = [single_dim_mask]
+    mask = np.stack(all_dims, axis=2)
+
+    return mask > 0.5
+
+
+def get_patched_img_size():
+    img_size = img_rows * img_cols
+    return img_size // 4  # patch_size_x*patch_size_y = 2*2 = 4
+
+
+def get_random_binomial(p, patched_img_size):
+    rand_matrix = np.random.binomial(1, p, size=patched_img_size)
+    return rand_matrix.reshape(img_rows // 2, img_cols // 2)
+
+
+def get_expand_matrix(n):
+    expand_matrix = np.zeros((n, 2*n)) #won't work for rectangular imgs
+    for i in range(n):
+        expand_matrix[i, 2*i:2*i+2] = 1
+    return expand_matrix
+
+
+num_epochs = 10
+for i in range(num_epochs):
+    print("epoch {}/{}".format(i+1, num_epochs))
+    masked_x_train = mask_input(x_train)
+    model.fit_generator(datagen.flow(masked_x_train, y_train, batch_size=batch_size),
+            epochs=1,
+            steps_per_epoch=np.ceil(x_train.shape[0] / batch_size),
+            validation_data=(x_test, y_test),
+            callbacks=[EarlyStopping(monitor='val_loss', patience=20, verbose=1)
+                ]
+        )
