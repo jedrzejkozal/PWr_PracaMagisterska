4d0a1f25ce85dbdd7fa5794abfbc5a373e449a7c
jedrzejkozal <jedrzej.kozal@gmail.com>
2019-05-02 14:56:04 +0200

    comment for testing

--- ReNet/mnist.py
+++ ReNet/mnist.py
@@ -42,10 +42,10 @@ y_train = to_categorical(y_train, num_classes)
 y_test = to_categorical(y_test, num_classes)
 
 #just for testing
-x_train = x_train[:10]
-y_train = y_train[:10]
-x_test = x_test[:10]
-y_test = y_test[:10]
+#x_train = x_train[:10]
+#y_train = y_train[:10]
+#x_test = x_test[:10]
+#y_test = y_test[:10]
 
 
 datagen = ImageDataGenerator(width_shift_range=[-2.0, 0.0, 2.0])
