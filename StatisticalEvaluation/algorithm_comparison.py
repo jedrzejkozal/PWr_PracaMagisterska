17118b984d415240dcbff49a7324e6c83a604e7f
jedrzejkozal <jedrzej.kozal@gmail.com>
2019-05-30 02:24:28 +0200

    docs update

--- StatisticalEvaluation/algorithm_comparison.py
+++ StatisticalEvaluation/algorithm_comparison.py
@@ -1,25 +1,54 @@
-from StatisticalAnalysis import *
+#from StatisticalAnalysis import *
+from saveTexTable import *
+import numpy as np
 
 #results from datasets
 #every row is results from difrrent folds
 #rows are in order: ReNet, ModifiedReNet, Conv
 x_ray = np.array([[0.9129692830730217, 0.9087030718757838, 0.9266211600026987, 0.9264957266995031, 0.9205128206147088],
-                [0.9325938570621477, 0.9291808877788713, 0.9308873724205096, 0.9247863249901014, 0.938461538665315],
-                [0.0, 0.0, 0.0, 0.0, 0.0]])
-print("x_ray = ", x_ray)
+                            [0.9325938570621477, 0.9291808877788713, 0.9308873724205096, 0.9247863249901014, 0.938461538665315],
+                            [0.9445392489433289, 0.955631399520835, 0.9411262798634812, 0.9495726491650964, 0.9589743585668058]])
 
 flowers = np.array([[0.5323325635103926, 0.5219399538106235, 0.5526011560693641, 0.5549132947976878, 0.5331010444654405],
-                [0.5554272517321016, 0.5369515011547344, 0.5399, 0.5930635838150289, 0.5435540062763689],
-                [0.0, 0.0, 0.0, 0.0, 0.0]])
+                            [0.5554272517321016, 0.5369515011547344, 0.5399, 0.5930635838150289, 0.5435540062763689],
+                            [0.7066974595842956, 0.6466512702078522, 0.6878612716763006, 0.6034682080924856, 0.6445993034127975]])
+
+fashion_mnist = np.array([[0.8707142857142857, 0.8708571428571429, 0.8611, 0.8694285713604518, 0.8736428572109768],
+                            [0., 0., 0., 0., 0.],
+                            [0., 0., 0., 0., 0.]])
+
+natural_images = np.array([[0.846710050657704, 0.8277858175693131, 0.841304347826087, 0.8511256354393609, 0.8474945533769063],
+                            [0.7715112075629824, 0.7532561505065123, 0.741304347826087, 0.7378358750907771, 0.7763253449527959],
+                            [0.9501084598698482, 0.9442836468023088, 0.9318840579710145, 0.9419026870007262, 0.9070442992011619]])
 
 #avrage accuracy across folds
-x_ray_avrg = np.mean(x_ray, axis=1).T
+x_ray_avrg = np.mean(x_ray, axis=1)
+flowers_avrg = np.mean(flowers, axis=1)
+fashion_avrg = np.mean(fashion_mnist, axis=1)
+natural_avrg = np.mean(natural_images, axis=1)
 print("x_ray_avrg = ", x_ray_avrg)
+print("flowers_avrg = ", flowers_avrg)
+print("fashion_avrg = ", fashion_avrg)
+print("natural_avrg = ", natural_avrg)
 
 #in rows are avrg results on dataset for given algorithm
 acc_avrg = np.array([[1, 10, 1, 1],
                     [20, 1, 20, 20],
                     [50, 50, 50, 50]])
 
-analysis = StatisticalAnalysis()
-statistic, pvalue, posthoc = analysis.testHypothesis(acc_avrg)
+acc_avrg = np.vstack([x_ray_avrg, flowers_avrg, fashion_avrg, natural_avrg])
+print(acc_avrg)
+
+print(list(acc_avrg))
+
+table_to_save = [[' ', 'ReNet', 'modif ReNet', 'conv'],
+                ['x_ray'] + list(acc_avrg[0]),
+                ['flowers'] + list(acc_avrg[1]),
+                ['fashion mnist'] + list(acc_avrg[2]),
+                ['natural images'] + list(acc_avrg[3])]
+
+print(table_to_save)
+save_tex_table(table_to_save, 'cross_validation')
+
+#analysis = StatisticalAnalysis()
+#statistic, pvalue, posthoc = analysis.testHypothesis(acc_avrg)
