a938de4a6bd184aaff65ffb8d03ff13b0e7860bc
jedrzejkozal <jedrzej.kozal@gmail.com>
2019-06-10 20:17:49 +0200

    removed uneceseary files

--- HyperparamsSearch/Tests/Mnist_GridSearch_test.py
+++
@@ -1,8 +0,0 @@
-from GridSearch import *
-from Tests.MnistModel import *
-
-
-sut = GridSearch()
-hyperparams_arg = {"lr": [0.01, 0.001]}
-modelAdapter = get_mnist_model()
-sut.search_hyperparms(modelAdapter, hyperparams_arg)
