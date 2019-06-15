e8ab524c4695ed44e923b4d393f8c67eec42911a
jedrzejkozal <jedrzej.kozal@gmail.com>
2019-06-15 21:51:22 +0200

    hilbert curve animation additional degrees

---
+++ animations/manimlib/HilbertCurve.py
@@ -0,0 +1,89 @@
+import numpy as np
+
+from CommandsGenerator import *
+
+class HilbertCurve1(object):
+
+    def __init__(self):
+        self.left_rotation_matrix = np.array([[0, -1], [1, 0]])
+        self.right_rotation_matrix = np.array([[0, 1], [-1, 0]])
+
+
+    def convert_2D_to_1D(self, img):
+        vec = self.__traverse_img(img)
+        return np.array(vec)
+
+
+    def get_indexes_vec(self, side_length):
+        dummy_img = self.__get_dummy_img(side_length)
+        return self.__traverse_img(dummy_img)
+
+
+    def __get_dummy_img(self, side_length):
+        result = []
+
+        for i in range(side_length):
+            for j in range(side_length):
+                result.append([i, j])
+
+        result_matrix = np.array(result)
+        return result_matrix.reshape((side_length, side_length, 2))
+
+
+    def __traverse_img(self, img):
+        self.__check_shape(img)
+
+        self.direction = np.reshape(np.array([[1], [0]]), (2,1))
+        self.curr_position = np.array([[0], [img.shape[0]-1]])
+
+        curve_deegre = np.log2(img.shape[0])
+        commands = self.__generate_commands(curve_deegre)
+
+        return self.__execute_all_commands(commands, img)
+
+
+    def __check_shape(self, img):
+        if img.shape[0] != img.shape[1] or not self.__is_power_of_2(img.shape[0]):
+            raise ValueError("Invalid img shape: {}".format(img.shape))
+
+
+    def __is_power_of_2(self, number):
+        return np.equal(np.mod(np.log2(number), 1), 0)
+
+
+    def __generate_commands(self, deegre):
+        g = CommandsGenerator()
+        return g.generate_commands(deegre)
+
+
+    def __execute_all_commands(self, commands, img):
+        result = []
+        result.append(img[self.curr_position[1], self.curr_position[0]])
+
+        for command in commands:
+            self.__execute_single_command(command, img, result)
+
+        return result
+
+
+    def __execute_single_command(self, command, img, result):
+        if command is "left":
+            self.__rotate_left()
+        elif command is "right":
+            self.__rotate_right()
+        else: # "F"
+            self.__move_forward()
+            result.append(img[self.curr_position[1], self.curr_position[0]])
+
+
+    def __rotate_left(self):
+        self.direction = np.matmul(self.left_rotation_matrix, self.direction)
+
+
+    def __rotate_right(self):
+        self.direction = np.matmul(self.right_rotation_matrix, self.direction)
+
+
+    def __move_forward(self):
+        self.curr_position[0] += self.direction[0]
+        self.curr_position[1] -= self.direction[1]
--- animations/manimlib/hilbert.py
+++ animations/manimlib/hilbert.py
@@ -1,35 +1,37 @@
+import sys
+sys.path.insert(0,'/home/jkozal/Dokumenty/PWr/magisterka/magisterka/animations/manimlib/')
+from HilbertCurve import *
+
 from manimlib.imports import *
 
 
 class Hilbert(Scene):
 
-    def get_basic_cure_at(self, point):
-        h1 = VMobject(color=BLACK)
-        h1.set_points([[-1,-1,0], [-1,1,0], [1,1,0], [1,-1,0]])
-        h1.set_points_as_corners([[-1,-1,0], [-1,1,0], [1,1,0], [1,-1,0]])
-        h1.set_x(point[0])
-        h1.set_y(point[1])
-        return h1
+    def setup(self):
+        self.camera.set_frame_width(self.camera.get_frame_width()+46)
+        self.camera.set_frame_height(self.camera.get_frame_height()+25)
+
 
     def construct(self):
-        base_position = np.array([-5,-2,0])
-        h0 = self.get_basic_cure_at(base_position)
+        base_position = np.array([-26,-14,0])
+        h0 = self.get_curve_at(2, base_position)
+        side_length = 0
 
-        max_deg = 2
-        for degree in range(1,max_deg):
-            side_length = 2*(2*degree-1)
+        max_deg = 3
+        for degree in range(1,max_deg+1):
+            side_length = side_length*2 + 2 #2*(2*degree-1)
             move_to_next_deegre = np.array([side_length+1,0,0])
-            move_up = np.array([0,2*side_length,0])
-            move_right = np.array([2*side_length,0,0])
-            move_down = np.array([0,-2*side_length,0])
+            move_up = np.array([0,side_length+2,0])
+            move_right = np.array([side_length+2,0,0])
+            move_down = np.array([0,-1*side_length-2,0])
 
-            h1 = self.get_basic_cure_at(base_position)
+            h1 = self.get_curve_at(2**degree, base_position)
             position_1st = base_position + move_to_next_deegre
-            h2 = self.get_basic_cure_at(position_1st)
+            h2 = self.get_curve_at(2**degree, position_1st)
             position_2nd = position_1st + move_up
-            h3 = self.get_basic_cure_at(position_2nd)
+            h3 = self.get_curve_at(2**degree, position_2nd)
             position_3rd = position_2nd + move_right
-            h4 = self.get_basic_cure_at(position_3rd)
+            h4 = self.get_curve_at(2**degree, position_3rd)
             position_4th = position_3rd + move_down
             connect1_2 = Line(position_1st+np.array([side_length/-2,side_length/2,0]), position_2nd+np.array([side_length/-2,side_length/-2,0]), color=BLACK)
             connect2_3 = Line(position_2nd+np.array([side_length/2,side_length/-2,0]), position_3rd+np.array([side_length/-2,side_length/-2,0]), color=BLACK)
@@ -44,8 +46,39 @@ class Hilbert(Scene):
             self.play(Rotate(h1, angle=PI/-2.0), Rotate(h4, angle=PI/2.0))
             self.play(ShowCreation(connect1_2), ShowCreation(connect2_3), ShowCreation(conncet3_4))
 
-            base_position = self.get_middle_point(position_1st, position_4th)
+            base_position = self.get_middle_point(position_1st, position_3rd)
+
+
+    def get_curve_at(self, side_length, point):
+        h1 = VMobject(color=BLACK)
+        h1.set_points(self.get_hilbert_curve(side_length))
+        h1.set_points_as_corners(self.get_hilbert_curve(side_length))
+        h1.set_x(point[0])
+        h1.set_y(point[1])
+        h1.rotate(PI/2)
+        return h1
+
+
+    def get_hilbert_curve(self, side_length):
+        indexes = self.get_indexes(side_length)
+        hilbert_curve = HilbertCurve1()
+        points = hilbert_curve.convert_2D_to_1D(indexes)
+        points = points + np.array([-side_length/4,-side_length/4])
+        points = points * 2
+        points = list(map(lambda x: np.append(x, [0]), points))
+        return points
+
+
+    def get_indexes(self, side_length):
+        result = []
+
+        for i in reversed(range(side_length)):
+            for j in range(side_length):
+                result.append([i, j])
+
+        result_matrix = np.array(result)
+        return result_matrix.reshape((side_length, side_length, 2))
 
 
     def get_middle_point(self, p1, p2):
-        return np.array([(p1[0]+p2[0])/2, (p1[1]+p2[1])/2])
+        return np.array([(p1[0]+p2[0])/2, (p1[1]+p2[1])/2, 0])
